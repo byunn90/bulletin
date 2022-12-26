8dad6d05318c404fc697928fe2bc6fbb54b254ae
byunn90 <kayhanturk90@Live.com>
2022-12-27 00:04:12 +1100

    doing the colors with time and present

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var hours = $("time-block");
+console.log(hours);
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -15,9 +17,46 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
+  // TODO: Add code to get any user input that was saved in localStorage and set ✅
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+$(".saveBtn").on("click", function () {
+  // jquery $(this) element who initiated event
+  var getValue = $(this).siblings("textarea").val();
+
+  var getTime = $(this).parent().attr("id");
+
+  localStorage.setItem(getTime, getValue);
+});
+
+$(".description").each(function (index) {
+  // We wanna grab value from local storage based on id
+  var getTime = $(this).parent().attr("id");
+
+  var gettingValue = localStorage.getItem(getTime);
+
+  $(this).val(gettingValue);
+});
+// ⬇ This gonna change all the colors for calender
+$(".time-block").each(function (idex) {
+  // get current hour
+  // get row time
+  var time = dayjs().hour();
+  var currentTime = $(".time-block");
+  if (currentTime === dayjs) {
+    console.log("Hello");
+  }
+});
+
+function realTime() {
+  const currentDay = $("#currentDay");
+
+  const dateOfMonth = dayjs().day(2).hour(1);
+
+  currentDay.text(dateOfMonth);
+}
+realTime();
